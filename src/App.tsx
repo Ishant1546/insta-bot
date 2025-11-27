385c7953fd334c15dd681b9e153b920355fdfb90
Ishant1546 <ishantchannel154@gmail.com>
2025-11-27 23:44:57 +0530

    full

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./pages/Dashboard";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-[#0a0a0a] text-white p-8">
-      <Dashboard />
-    </div>
-  );
-}
